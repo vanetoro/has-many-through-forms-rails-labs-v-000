fb82bfd6c07ca32a695b90869c9a79ca00104a53
Vanessa Toro <vanetoro84@gmail.com>
2018-07-10 17:43:29 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments
+    @categories = @post.categories
   end
 
   def index
